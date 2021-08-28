5042b2e283b54613a3711cd21a3f1b37e3b33b52
Daniel Schmitz <danielschmitz.arts@gmail.com>
2021-08-28 21:29:43 +0200

    Refactoring

--- producer/mockdata.js
+++ producer/mockdata.js
@@ -1,42 +1,44 @@
 const fetch = require("fetch");
 const parser = require('xml2json');
 
-async function getMockData() {
-  function getProduct(oEntry) {
+function parseProduct(oEntry) {
     return {
-      product_number: oEntry.content["m:properties"]["d:ProductID"].$t,
-      product_title: oEntry.content["m:properties"]["d:ProductName"]
+        product_number: oEntry.content["m:properties"]["d:ProductID"].$t,
+        product_title: oEntry.content["m:properties"]["d:ProductName"]
     }
-  }
+}
 
-  const aProducts = await new Promise(resolve => {
-    fetch.fetchUrl("https://services.odata.org/V3/Northwind/Northwind.svc/Products", function (error, meta, body) {
-      const oResponse = parser.toJson(body.toString(), {object: true});
-      resolve(oResponse.feed.entry.map(getProduct));
-    });
-  });
-
-  function getOrder(oEntry) {
+function parseOrder(oEntry, aProducts) {
     const iId = oEntry.content["m:properties"]["d:ProductID"].$t;
     const oProduct = aProducts.find(e => e.product_number === iId);
     if (oProduct) {
-      return {
-        product_number: iId,
-        product_title: oProduct.product_title,
-        price: oEntry.content["m:properties"]["d:UnitPrice"].$t,
-        quantity: oEntry.content["m:properties"]["d:Quantity"].$t,
-        timestamp: new Date().toISOString()
-      };
+        return {
+            product_number: iId,
+            product_title: oProduct.product_title,
+            price: oEntry.content["m:properties"]["d:UnitPrice"].$t,
+            quantity: oEntry.content["m:properties"]["d:Quantity"].$t,
+            timestamp: new Date().toISOString()
+        };
     }
-  }
+}
 
-  return await new Promise(resolve => {
-    fetch.fetchUrl("https://services.odata.org/V3/Northwind/Northwind.svc/Order_Details", function (error, meta, body) {
-      const oResponse = parser.toJson(body.toString(), {object: true});
-      const oProcessedResponse = oResponse.feed.entry.map(getOrder).filter(e => !!e);
-      resolve(oProcessedResponse);
+async function getMockData() {
+    const aProducts = await new Promise(resolve => {
+        fetch.fetchUrl("https://services.odata.org/V3/Northwind/Northwind.svc/Products", function (error, meta, body) {
+            const oResponse = parser.toJson(body.toString(), {object: true});
+            resolve(oResponse.feed.entry.map(parseProduct));
+        });
     });
-  });
+
+    return await new Promise(resolve => {
+        fetch.fetchUrl("https://services.odata.org/V3/Northwind/Northwind.svc/Order_Details", function (error, meta, body) {
+            const oResponse = parser.toJson(body.toString(), {object: true});
+
+            let aProcessedResponse = oResponse.feed.entry.map(e => parseOrder(e, aProducts));
+            aProcessedResponse = aProcessedResponse.filter(e => !!e);
+            resolve(aProcessedResponse);
+        });
+    });
 }
 
 module.exports = getMockData
